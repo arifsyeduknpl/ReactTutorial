c5a4c8d3fa22792290024ddf050dc211a7150660
arifsyeduknpl <arif.syed@hotmail.co.uk>
2021-06-20 16:51:51 +0100

    Section - DB Connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,9 @@
 const db={
-'user': 'books',
-'password': 'books'
+'user': 'books@npl-mysql-arif-dev',
+'password': 'books',
+'port': '3306',
+'database': 'books',
+'host': 'npl-mysql-arif-dev.mysql.database.azure.com'
 }
 
 module.exports = db
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,14 +1,23 @@
 var express = require('express');
+const mysql = require('mysql');
+const fs = require('fs');
 var router = express.Router();
 
 // read in db.js and print out values
 const db = require('../config/db');
 console.log(db.user, db.password);
 
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('books');
+
+  const conn = mysql.createConnection(db);
+  conn.connect(err => {
+    if (err) throw err;
+    console.log('Connected');
+  });
+  res.send('books for sale');
+
+  
 });
 
 module.exports = router;
