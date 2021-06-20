507641a336c68b097bf276fd7413f4ced0fa2e98
arifsyeduknpl <arif.syed@hotmail.co.uk>
2021-06-20 17:35:16 +0100

    Section 3 connection Pooling

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,23 +1,34 @@
 var express = require('express');
 const mysql = require('mysql');
-const fs = require('fs');
 var router = express.Router();
 
+const connectionPool = require('../database/connection-pool')
+
 // read in db.js and print out values
-const db = require('../config/db');
-console.log(db.user, db.password);
+// now in server/bin/www file
+// const db = require('../config/db');
+// console.log(db.user, db.password);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  const conn = mysql.createConnection(db);
-  conn.connect(err => {
-    if (err) throw err;
-    console.log('Connected');
-  });
-  res.send('books for sale');
+  //const conn = mysql.createConnection(db);
+  //conn.connect(err => {
+  //  if (err) throw err;
+  //  console.log('Connected');
 
-  
+    const book = {
+      author: 'Arif Syed',
+      title: 'bikes2',
+      published: '2021-08-19'
+    }
+
+    connectionPool.getPool().query('insert into books set ?', book, (err, result) => {
+      if(err) throw err;
+      console.log('1 record inserted');
+    });
+
+    res.send('books for sale');
 });
 
 module.exports = router;
