ab77e7b1be3865d0e0b882cbe9a9de1ca68b356c
arifsyeduknpl <arif.syed@hotmail.co.uk>
2021-06-20 11:42:42 +0100

    Section 3 Creating a Component

---
+++ client/src/Book.js
@@ -0,0 +1,11 @@
+//import React from 'react'; included in index.js
+import './Book.css';
+
+function Book() {
+    return(
+            <div id='book-message'>
+                 Hello Book   
+            </div>
+    )
+}
+export default Book;
